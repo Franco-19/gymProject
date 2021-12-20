d1b1243240ff352d343ac948b3f0c2d699d28575
German Drusetich <germandrusetich@gmail.com>
2021-12-20 10:55:57 -0300

    creandoFormulario

--- resources/views/usuarios.blade.php
+++ resources/views/usuarios.blade.php
@@ -1,4 +1,13 @@
 <h1>{{$titulo}}}</h1>
+<form method="get" action ="usuarios">
+    <label>Nro Socio: </label><input type ="number" name="id"><br>
+    <label>Nombre: </label><input type ="text" name="nombre"><br>
+    <label>Apellido: </label><input type ="text" name="apellido"><br>
+    <label>Rutina: </label><input type ="text" name="nombre"><br>
+<button type = "submit">Filtrar</button>
+
+
+</form>
 <table>
     <thead>
         <tr>
